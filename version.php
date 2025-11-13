d668fa16aedc9a8efce600dbf836590f44409fa5
Petr Skoda <petr@mutms.org>
2025-11-13 10:11:34 +0100

    Improve tenant switching

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutenancy';
-$plugin->version = 2025110850;
+$plugin->version = 2025110850.01;
 $plugin->requires = 2025041401;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 500];
