db1c64826c5f36b40df1a9518615f8dec9fc504f
Petr Skoda <petr@mutms.org>
2025-12-07 11:17:35 +0100

    Prepare 5.1.1 release

--- version.php
+++ version.php
@@ -28,13 +28,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutenancy';
-$plugin->version = 2025120351;
-$plugin->requires = 2025100600;
+$plugin->version = 2025120851;
+$plugin->requires = 2025100601;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [501, 501];
 $plugin->incompatible = 502;
-$plugin->release = 'mu-5.1.0-02+';
+$plugin->release = 'mu-5.1.1-01';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025120350,
+    'tool_mulib' => 2025120850,
 ];
