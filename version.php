0fd5fa39b4cf41d60d8e84f960adc39b1130189b
Petr Skoda <petr@mutms.org>
2026-09-18 10:42:32 +0200

    Back to dev

--- version.php
+++ version.php
@@ -28,12 +28,12 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutenancy';
-$plugin->version = 2026091345;
+$plugin->version = 2026091845;
 $plugin->requires = 2024100714;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
-$plugin->release = 'v4.5.14.01';
+$plugin->release = 'v4.5.14.01+';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026091345,
+    'tool_mulib' => 2026091845,
 ];
